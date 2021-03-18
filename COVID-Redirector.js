e7d811946be6ff55f34f0dc7ef29a7bdb9bdacdd
Alexander Martin <Alexander@theAlexanderMartin.com>
2021-03-18 17:16:46 -0400

    Initial Commit My first chrome extension. I got fed up after two semesters of filling this out every time my auth token expired. I wish I had know how little effort was required to make a chrome extension. They can literally just be a javascript file with a manifest.

---
+++ COVID-Redirector.js
@@ -0,0 +1,26 @@
+const BLOCKED = "https://sso.mtu.edu/cas/canvas.html";
+const SYMPTOM_FORM = "https://www.banweb.mtu.edu/owassb/symptom_form.p_form";
+const FILLED_FORM = "https://www.banweb.mtu.edu/owassb/symptom_form.p_submit?symptoms=NONE&contact_with=N";
+const CANVAS = "https://mtu.instructure.com";
+
+
+console.log("COVID Form Redirector loaded.");
+console.log("window.location.herf is " + window.location.href);
+switch (window.location.href) {
+    case BLOCKED:
+        console.log("Looks like canvas has been blocked. Redirecting.");
+        location.replace(FILLED_FORM);
+        break;
+    case SYMPTOM_FORM:
+        console.log("Page is a the COVID Form, redirecting.");
+        location.replace(FILLED_FORM);
+        break;
+    case FILLED_FORM:
+        console.log("Form submitted sucessfully. Redirecting to Canvas");
+        location.replace(CANVAS);
+        break;
+    default:
+        console.log("URL did not match something we care about.");
+        break;
+}
+
---
+++ README.md
@@ -0,0 +1,16 @@
+# MTU COVID Form Filler
+
+Since I have no in person classes, it always annoys me that I have to fill out this form to be able to access my online 
+classes, even though I won't step foot on campus until the fall. It's especially annoying when trying to submit an
+assignment last-minute, only to have to fill out a form and login again.
+
+This extension automatically redirects you when your canvas access is blocked. It fills out the form, then brings you to
+the canvas homepage.
+
+## Installation
+
+ Google now blocks crx extensions from outside their own store. Luckily, you can work around this by enabling developer
+ mode and loading an unpacked extension.
+ 
+Go to [chrome://extensions](chrome://extensions), and click the switch in the top right to enable developer mode. Then,
+drag the entire folder onto the window to install it.
