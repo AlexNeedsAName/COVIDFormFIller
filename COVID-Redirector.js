1be9a6f7be24fa8626ac2e07904fc3695cb7d320
Alexander Martin <Alexander@theAlexanderMartin.com>
2021-03-26 15:38:50 -0400

    Now deal with the login redirection When you have to login to banweb to fill out the form, it discards the query string and submits an empty form. Now, when you attempt to submit an empty form, it redirects once again to the filled form.

--- COVID-Redirector.js
+++ COVID-Redirector.js
@@ -1,8 +1,8 @@
 const BLOCKED = "https://sso.mtu.edu/cas/canvas.html";
 const SYMPTOM_FORM = "https://www.banweb.mtu.edu/owassb/symptom_form.p_form";
 const FILLED_FORM = "https://www.banweb.mtu.edu/owassb/symptom_form.p_submit?symptoms=NONE&contact_with=N";
+const REDIRECTED_FORM = "https://www.banweb.mtu.edu/owassb/symptom_form.p_submit"
 const CANVAS = "https://mtu.instructure.com";
-
 
 console.log("COVID Form Redirector loaded.");
 console.log("window.location.herf is " + window.location.href);
@@ -13,6 +13,10 @@ switch (window.location.href) {
         break;
     case SYMPTOM_FORM:
         console.log("Page is a the COVID Form, redirecting.");
+        location.replace(FILLED_FORM);
+        break;
+    case REDIRECTED_FORM:
+        console.log("Login process discarded query string. Redirecting.");
         location.replace(FILLED_FORM);
         break;
     case FILLED_FORM:
